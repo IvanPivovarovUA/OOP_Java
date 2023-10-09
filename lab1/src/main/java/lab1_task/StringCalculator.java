b4542162bd2f52dc8054541661ece8def3804966
Ivan Pivovarov <ivanpivovarovua@gmail.com>
2023-10-09 20:12:38 +0300

    lab 1 task 4

--- lab1/src/main/java/lab1_task/StringCalculator.java
+++ lab1/src/main/java/lab1_task/StringCalculator.java
@@ -4,8 +4,19 @@ public class StringCalculator {
 
     public int add(String numbers) {
         int sum = 0;
-        String string_numbers[] = numbers.split(",|\\\\n");
 
+        String new_sep = "";
+        String new_num = numbers;
+        if (numbers.length() > 2 && numbers.indexOf("\\n") > 2)
+        {
+            if (numbers.charAt(0) == '/' && numbers.charAt(1) == '/') {
+                new_sep = cut_separato(numbers);
+                new_num = cut_number(numbers);
+            }
+        }
+        // System.out.println("{" + new_sep + "}~{" + new_num + "}");
+
+        String string_numbers[] = new_num.split(",|\\\\n" + new_sep);
         for (String i: string_numbers) {
             try
             {
@@ -20,4 +31,20 @@ public class StringCalculator {
         return sum;
     }
 
+
+    String cut_separato(String user_string) {
+        String new_sep = user_string.substring(
+            2,
+            user_string.indexOf("\\n")
+        );
+        new_sep = "|\\Q" + new_sep + "\\E";
+        return new_sep;
+    }
+    String cut_number(String user_string) {
+        String new_num = user_string.substring(
+            user_string.indexOf("\\n") + 2,
+            user_string.length()
+        );
+        return new_num;
+    }
 }
--- lab1/src/test/java/lab1_task/StringCalculatorTest.java
+++ lab1/src/test/java/lab1_task/StringCalculatorTest.java
@@ -22,4 +22,23 @@ public class StringCalculatorTest {
         Assertions.assertEquals(44, Calc.add("44,gtg"));
     }
 
+    @Test
+    void TwoSeparators() {
+        System.out.println("~TEST TwoSeparator~");
+        StringCalculator Calc = new StringCalculator();
+
+        Assertions.assertEquals(144, Calc.add("100\\n44"));
+        Assertions.assertEquals(204, Calc.add("100\\n44,60"));
+    }
+
+    @Test
+    void NewSeparator() {
+        System.out.println("~TEST NewSeparator~");
+        StringCalculator Calc = new StringCalculator();
+
+        Assertions.assertEquals(27, Calc.add("//#\\n4#23"));
+        Assertions.assertEquals(70, Calc.add("//#\\n4#23,43"));
+        Assertions.assertEquals(39, Calc.add("//435,34,5"));
+    }
+
 }
