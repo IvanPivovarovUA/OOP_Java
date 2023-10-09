9cdf9b383a7c6425cc5217f0e9d19db1a9718744
Ivan Pivovarov <ivanpivovarovua@gmail.com>
2023-10-09 18:01:12 +0300

    lab 1 task 2

--- lab1/src/main/java/lab1_task/StringCalculator.java
+++ lab1/src/main/java/lab1_task/StringCalculator.java
@@ -6,23 +6,14 @@ public class StringCalculator {
         int sum = 0;
         String string_numbers[] = numbers.split(",");
 
-        int first_task_limit = 2;
-        if (string_numbers.length > 2) {
-            System.out.println("Work only with two numbers");
-        }
-
         for (String i: string_numbers) {
             try
             {
                 sum += Integer.parseInt(i);
-                first_task_limit -= 1;
             }
             catch (NumberFormatException nfe)
             {
                 System.out.println("NumberFormatException: " + nfe.getMessage());
-            }
-            if (first_task_limit <= 0) {
-                break;
             }
         }
 
