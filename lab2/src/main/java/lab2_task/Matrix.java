80ebc1aca7e5dcfe038c39bab05dfc69447a1523
Ivan Pivovarov <ivanpivovarovua@gmail.com>
2023-11-03 14:20:38 +0200

    lab 2 task 2

--- lab2/src/main/java/lab2_task/Matrix.java
+++ lab2/src/main/java/lab2_task/Matrix.java
@@ -6,6 +6,56 @@ import java.util.ArrayList;
 //  https://www.baeldung.com/java-multi-dimensional-arraylist
 public class Matrix {
     private ArrayList<ArrayList<Integer>> Elements;
+
+    public void printElements() {
+        if (Elements.size() != 0) {
+            System.out.println("\n[");
+            for (ArrayList<Integer> elem: Elements){
+                System.out.println(elem);
+            }
+            System.out.println("]\n");
+        }else {
+            System.out.println("\n[]\n");
+        }
+
+    }
+
+    Matrix() {
+        Elements = new ArrayList<ArrayList<Integer>>();
+    }
+    Matrix(int row, int column) {
+        Elements = new ArrayList<ArrayList<Integer>>();
+
+        row = Math.abs(row);
+        column = Math.abs(column);
+        if (row != 0 && column != 0) {
+            for (int c=0; c < column; c++) {
+                Elements.add(new ArrayList<Integer>());
+
+                for (int r=0; r < row; r++) {
+                    Elements.get(c).add(0);
+                }
+            }   
+        }
+    }
+    Matrix(Matrix matrix) {
+        Elements = new ArrayList<ArrayList<Integer>>();
+
+        if (matrix.Elements.size() != 0) {
+            int row = matrix.Elements.get(0).size();
+            int column = matrix.Elements.size();
+
+            for (int c=0; c < column; c++) {
+                Elements.add(new ArrayList<Integer>());
+
+                for (int r=0; r < row; r++) {
+                    Elements.get(c).add(
+                        matrix.Elements.get(c).get(r)
+                    );
+                }
+            }
+        }
+    }
 }
 
 
--- lab2/src/main/java/lab2_task/app.java
+++ lab2/src/main/java/lab2_task/app.java
@@ -7,5 +7,17 @@ public class app {
     */
     public static void main(String[] args) {
         Matrix matrix1 = new Matrix();
+        Matrix matrix2 = new Matrix(-4,0);
+        Matrix matrix3 = new Matrix(0,13);
+        Matrix matrix4 = new Matrix(2,-15);
+        Matrix matrix5 = new Matrix(matrix1);
+        Matrix matrix6 = new Matrix(matrix2);
+
+        matrix1.printElements();
+        matrix2.printElements();
+        matrix3.printElements();
+        matrix4.printElements();
+        matrix5.printElements();
+        matrix6.printElements();
     }
 }
