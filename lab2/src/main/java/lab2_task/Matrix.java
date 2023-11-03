08f4b966725eff35db8fa8f377f341ced5183b70
Ivan Pivovarov <ivanpivovarovua@gmail.com>
2023-11-03 14:19:35 +0200

    Start new lab - lab 2 task 1

---
+++ lab2/src/main/java/lab2_task/Matrix.java
@@ -0,0 +1,12 @@
+package lab2_task;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+//  https://www.w3schools.com/java/java_arraylist.asp
+//  https://www.baeldung.com/java-multi-dimensional-arraylist
+public class Matrix {
+    private ArrayList<ArrayList<Integer>> Elements;
+}
+
+
+
---
+++ lab2/src/main/java/lab2_task/app.java
@@ -0,0 +1,11 @@
+package lab2_task; 
+
+public class app {
+    /*
+    gradle --console plain run
+    - run comand for Scanner
+    */
+    public static void main(String[] args) {
+        Matrix matrix1 = new Matrix();
+    }
+}
